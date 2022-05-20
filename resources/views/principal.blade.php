d162fc3f1d90bb2d95ade03d0c440af7174e495e
joao1419 <joao.santos1419@etec.sp.gov.br>
2022-05-20 17:48:47 -0300

    verdeagua

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<htmllang="pt">
+
+<head>
+
+<metacharset="utf-8">
+
+<title>Produtos</title>
+
+</head>
+
+<body>
+
+<center>
+
+<h1>Produtos</h1>
+
+<a href="/cadastrar"><buttontype="button"class="btn">Cadastrar</button></a>
+
+<a href="/listar-produto/1"><buttontype="button"class="btn">Listar</button></a>
+
+<a href="/editar-produto/1"><buttontype="button"class="btn">Atualizar</button></a>
+
+<a href="/excluir-produto/1"><buttontype="button"class="btn">Excluir</button></a>
+
+</center>
+
+</body>
+
+</html>
